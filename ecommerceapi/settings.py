744bb08d3f1c42fddb5c70055b65ed3cb7ca2e55
Md-Salman-Rahman339 <salmanrahman339@gmail.com>
2025-06-24 21:01:46 +0600

    Users admin.py UserModelAdmin register from documentation

--- ecommerceapi/settings.py
+++ ecommerceapi/settings.py
@@ -143,6 +143,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL='Users.MyUser' 
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
