0f68df45b8490be63337d5208986002dc0fdd03f
Md-Salman-Rahman339 <salmanrahman339@gmail.com>
2025-06-25 14:54:12 +0600

    implement password change view

--- Users/serializers.py
+++ Users/serializers.py
@@ -35,3 +35,20 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model=MyUser
         fields=['id','email','name']
+
+class UserChangePasswordSerializer(serializers.Serializer):
+      password = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
+      password2 = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
+
+      class Meta:
+          fields=['password','password2']
+
+      def validate(self, attrs):
+        password=attrs.get('password')
+        password2=attrs.get('password2')
+        user=self.context.get('user')
+        if password !=password2:
+            raise serializers.ValidationError("Password and confirm Password doesn't match")
+        user.set_password(password)
+        user.save()
+        return attrs  
--- Users/views.py
+++ Users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import UserRegistrationSerializer,UserLoginSerializers,UserProfileSerializer
+from .serializers import UserRegistrationSerializer,UserLoginSerializers,UserProfileSerializer,UserChangePasswordSerializer
 from django.contrib.auth import authenticate
 from .renderers import UserRenderer
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -53,3 +53,11 @@ class UserProfileView(APIView):
     def get(self,request,formate=None):
         serializer=UserProfileSerializer(request.user)
         return Response(serializer.data,status=status.HTTP_200_OK)
+    
+class UserPasswordChangeView(APIView):
+    renderer_classes=[UserRenderer]
+    permission_classes=[IsAuthenticated]
+    def post(self,request,formate=None):
+        serializer=UserChangePasswordSerializer(data=request.data,context={'user':request.user})
+        serializer.is_valid(raise_exception=True)
+        return Response({'msg':'Password Changed Successfully'}, status=status.HTTP_200_OK)
